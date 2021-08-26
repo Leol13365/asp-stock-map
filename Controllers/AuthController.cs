eb48abeb65579d34bbdbd5230f8dea3cc19b8884
Leon WU <leol13365@ymail.com>
2021-08-27 04:29:32 +0800

    Create Auth

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StockMap.Models;
+
+namespace StockMap.Controllers
+{
+    public class AuthController : Controller
+    {
+        private StockMapDbContext db = new StockMapDbContext();
+
+        // GET: Auth/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Auth/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login([Bind(Include = "Account,Password")] User data)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = db.Users.Where(m => m.Account == data.Account).FirstOrDefault();
+                if(user == null){
+                    ModelState.AddModelError(nameof(data.Account), "此帳號不存在，請先註冊");
+                    return View(data);
+                }
+                if (data.Password != user.Password)
+                {
+                    ModelState.AddModelError(nameof(data.Password), "密碼錯誤");
+                    return View(data);
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View(data);
+        }
+
+        // GET: Auth/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Auth/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "Account,Password,ConfirmedPassword")] RegisterUser data)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = db.Users.Where(m => m.Account == data.Account).FirstOrDefault();
+                if(user != null) {
+                    ModelState.AddModelError(nameof(data.Account), "此帳號已有人使用");
+                    return View(data); 
+                }
+                db.Users.Add(data);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+
+            return View(data);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -16,14 +16,31 @@ namespace StockMap.Models
         }
 
         [Key]
+        [Required]
         [StringLength(20)]
+        [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "度璣ゅダの计")]
         public string Account { get; set; }
 
         [Required]
         [StringLength(20)]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "度璣ゅダの计")]
         public string Password { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Favorite> Favorites { get; set; }
     }
+
+    [NotMapped]
+    public partial class RegisterUser:User
+    {
+        [NotMapped]
+        [Required]
+        [StringLength(20)]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "ゲ惠籔Password")]
+        [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "度璣ゅダの计")]
+        public string ConfirmedPassword { get; set; }
+    }
+
 }
