0e6d933204282da342633f306424e010012d1bbc
Leon WU <leol13365@ymail.com>
2021-09-03 01:19:36 +0800

    Create FavoriteViewModel

---
+++ Models/FavoriteViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StockMap.Models
+{
+    public class FavoriteViewModel
+    {
+        public StockMap.Models.Favorite FavoriteCreate { get; set; }
+        public IEnumerable<StockMap.Models.Favorite> FavoriteIndex { get; set; }
+    }
+}
