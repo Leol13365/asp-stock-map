913f1ed1576e40c53f6b71129af653abc273a7e8
Leon WU <leol13365@ymail.com>
2021-09-03 05:37:02 +0800

    Create StockSearch

---
+++ Controllers/StocksController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StockMap.Models;
+
+namespace StockMap.Controllers
+{
+    public class StocksController : Controller
+    {
+        private StockMapDbContext db = new StockMapDbContext();
+
+        // GET: Stocks
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Stocks/Index
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string searchId)
+        {
+            ViewData["CurrentFilter"] = searchId;
+            if (searchId == null){ ViewBag.ErroMsg = "請輸入股票代碼"; return View(); }
+            var stockId = db.Stocks.Where(m => m.StockId == searchId).FirstOrDefault();
+            if (stockId == null) { ViewBag.ErroMsg = "查無此股"; return View(); }
+            Stock stock = db.Stocks.Find(searchId);
+            ViewBag.CheckOk = true;
+            return View(stock);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
