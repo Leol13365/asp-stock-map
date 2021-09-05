e67c51d17c3d341ccbf298e520fd04c82f4fa2ee
Leon WU <leol13365@ymail.com>
2021-09-06 02:35:56 +0800

    feat(StockChips) create order、top10

--- Controllers/StockChipsController.cs
+++ Controllers/StockChipsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using StockMap.Models;
+using PagedList;
 
 namespace StockMap.Controllers
 {
@@ -15,10 +16,46 @@ namespace StockMap.Controllers
         private StockMapDbContext db = new StockMapDbContext();
 
         // GET: StockChips
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder)
         {
-            var stockChips = db.StockChips.Include(s => s.Stock);
-            return View(stockChips.ToList());
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.FINBSSortParm = sortOrder == "finbs_esc" ? "finbs_desc" : "finbs_esc";
+            ViewBag.ITCNBSSortParm = sortOrder == "itcnbs_esc" ? "itcnbs_desc" : "itcnbs_esc";
+            ViewBag.DNBSSortParm = sortOrder == "dnbs_esc" ? "dnbs_desc" : "dnbs_esc";
+            ViewBag.TSBSortParm = sortOrder == "tsb_esc" ? "tsb_desc" : "tsb_esc";
+            var stockChips = from s in db.StockChips
+                             select s;
+            switch (sortOrder)
+            {
+                case "finbs_desc":
+                    stockChips = stockChips.OrderByDescending(s => s.StockFINBS);
+                    break;
+                case "finbs_esc":
+                    stockChips = stockChips.OrderBy(s => s.StockFINBS);
+                    break;
+                case "itcnbs_desc":
+                    stockChips = stockChips.OrderByDescending(s => s.StockTCNBS);
+                    break;
+                case "itcnbs_esc":
+                    stockChips = stockChips.OrderBy(s => s.StockTCNBS);
+                    break;
+                case "dnbs_desc":
+                    stockChips = stockChips.OrderByDescending(s => s.StockDNBSProprietary);
+                    break;
+                case "dnbs_esc":
+                    stockChips = stockChips.OrderBy(s => s.StockDNBSProprietary);
+                    break;
+                case "tsb_desc":
+                    stockChips = stockChips.OrderByDescending(s => s.ThreeSellBuy);
+                    break;
+                case "tsb_esc":
+                    stockChips = stockChips.OrderBy(s => s.ThreeSellBuy);
+                    break;
+                default:
+                    stockChips = stockChips.OrderBy(s => s.StockId);
+                    break;
+            }
+            return View(stockChips.ToPagedList(1, 10));
         }
 
         protected override void Dispose(bool disposing)
