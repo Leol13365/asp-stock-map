8d854c7facd0a7001114a8955bb725b7df5e3d0a
Leon WU <leol13365@ymail.com>
2021-09-07 04:22:45 +0800

    feat(Stock): handle api response exception

--- Controllers/StocksController.cs
+++ Controllers/StocksController.cs
@@ -4,6 +4,8 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using StockMap.Models;
@@ -23,12 +25,30 @@ namespace StockMap.Controllers
         // POST: Stocks/Index
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Index(string searchId)
+        public async Task<ActionResult> Index(string searchId)
         {
             ViewData["CurrentFilter"] = searchId;
-            if (searchId == null){ ViewBag.ErroMsg = "請輸入股票代碼"; return View(); }
+            if (searchId == null) { ViewBag.ErroMsg = "請輸入股票代碼"; return View(); }
             var stockId = db.Stocks.Where(m => m.StockId == searchId).FirstOrDefault();
             if (stockId == null) { ViewBag.ErroMsg = "查無此股"; return View(); }
+            DateTime localDate = DateTime.UtcNow;
+            DateTime stockUpdate = db.Stocks.Find(searchId).UpdateTime;
+            System.TimeSpan between = localDate.Subtract(stockUpdate);
+            if (between.TotalMinutes > 1)
+            {
+                try
+                {
+                    string targetURL = "http://localhost:5000/api/v1/stock?stock_id=" + searchId;
+                    HttpClient client = new HttpClient();
+                    client.MaxResponseContentBufferSize = Int32.MaxValue;
+                    var response = await client.GetStringAsync(targetURL);
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+            
             Stock stock = db.Stocks.Find(searchId);
             ViewBag.CheckOk = true;
             return View(stock);
